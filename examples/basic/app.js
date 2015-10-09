d5a36823a5c5366199636afd8eef978870def2a8
Julien Bouquillon <julien@bouquillon.com>
2015-10-09 10:22:03 +0200

    add demo credentials

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -5,7 +5,7 @@ import AlgoliaInput from '../../lib/index';
 
 
 
-const algoliaClient = algoliasearch('APPLICATION_ID', 'SEARCH_ONLY_API_KEY');
+const algoliaClient = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
 
 class App extends Component {
   constructor(...args) {
@@ -26,7 +26,7 @@ class App extends Component {
     return (
       <div className="example">
         <h1>algolia-react-input</h1>
-        <AlgoliaInput client={ algoliaClient } index='test' onResults={ ::this.onResults } onError={ ::this.onError }/>
+        <AlgoliaInput client={ algoliaClient } index='instant_search' onResults={ ::this.onResults } onError={ ::this.onError }/>
         <hr/>
         { this.state.hits.map(hit => <li ref={ hit.objectID }>{ hit.name || hit.title }</li> ) }
       </div>
